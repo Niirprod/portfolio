2e02348d8f880ee393632cea59db11924c397cea
nabil <djanfar.nabil@gmail.com>
2019-10-28 12:05:54 +0100

    nav bar

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Calligraffitti&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Orbitron:400,500,700|Turret+Road:400,500,700&display=swap&subset=latin-ext" rel="stylesheet">
+	<meta charset="utf-8">
+	<title>Portfolio</title>
+</head>
+<body>
+		<header>
+		<div id="back"></div>	
+		<div id="fondmenu">
+			<ul class="menu">
+				<li> <a href="#">MMS</a></li>
+				<li> <a href="#">LDO</a></li>
+				<li> <a href="#">NRM</a></li>
+				<li> <a href="#">667</a></li>
+			</ul>
+		</div>
+		
+		<div>
+			<ul id="contact">
+				<li> <a href="#">Me contacter</a> </li>	
+			</ul>
+		</div>
+	<div >
+	<a href="#">
+	<img class="ico" src="img/home5.png"/>
+	</a>
+	</div>	
+	</header>
+
+<div>
+	<h1>Développeur web junior</h1>
+	<h2>Nabil DJANFAR</h2>
+</div>
+
+
+</body>
+</html>
