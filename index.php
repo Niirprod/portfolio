c706df5478d48f51224d5e8c787f381b99c63f4a
nabil <djanfar.nabil@gmail.com>
2019-11-08 09:43:50 +0100

    yo

--- index.php
+++ index.php
@@ -1,41 +1,76 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<link href="https://fonts.googleapis.com/css?family=Calligraffitti&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Orbitron:400,500,700|Turret+Road:400,500,700&display=swap&subset=latin-ext" rel="stylesheet">
 	<meta charset="utf-8">
 	<title>Portfolio</title>
+	<!-- link to js fontawsome -->
+	<script src="https://kit.fontawesome.com/f3b59627ec.js" crossorigin="anonymous"></script>
+	<!-- link to css -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!-- Font style -->
+	<link href="https://fonts.googleapis.com/css?family=Calligraffitti&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Orbitron:400,500,700|Turret+Road:400,500,700&display=swap&subset=latin-ext" rel="stylesheet">	
 </head>
 <body>
-		<header>
+	<header>
 		<div id="back"></div>	
 		<div id="fondmenu">
+			
+			<a href="#">		
+				<i id="logo" class="fab fa-jedi-order" title="Acceuil"></i>
+			</a>
+
 			<ul class="menu">
-				<li> <a href="#">MMS</a></li>
-				<li> <a href="#">LDO</a></li>
-				<li> <a href="#">NRM</a></li>
-				<li> <a href="#">667</a></li>
+				<li> <a href="#">A propos</a></li>
+				<li> <a href="#comp">Compétences</a></li>
+				<li> <a href="#">Mes projets</a></li>
+				<li> <a href="#"></a></li>
+				<li id="contact"> <a href="#">Me contacter</a> </li>	
 			</ul>
 		</div>
-		
-		<div>
-			<ul id="contact">
-				<li> <a href="#">Me contacter</a> </li>	
-			</ul>
-		</div>
-	<div >
-	<a href="#">
-	<img class="ico" src="img/home5.png"/>
-	</a>
-	</div>	
 	</header>
 
-<div>
-	<h1>Développeur web junior</h1>
-	<h2>Nabil DJANFAR</h2>
+<div class="top">
+	<h1 id="dwj">Développeur web junior</h1>
+	<h2 id="nd">Nabil DJANFAR</h2>
+</div>
+
+<h2 class="">A propos de mois</h2>
+
+	<p>Je m'appelle DJANFAR NAbil, j'ai 24 ans et je suis en formation développeur web à Simplon.</p>
+
+<div id="comp">
+<h2>Mes compétences</h2>
+
+<div class="ti">
+	<h3 id="fronti">Front end</h3> 
+
+	<h3 id="backti">Back end</h3>
+</div>
+
+<div class="si">
+	<div class="front">
+		<i id="html" class="fab fa-html5 ico" title="html"></i>
+		<i id="css" class="fab fa-css3-alt ico" title="css"></i>
+		<i id="js" class="fab fa-js ico" title="Javascript"></i> 
+	</div>
+
+	<div class="back">
+		<i id="php" class="fab fa-php ico" title="php"></i>
+		<i id="sf" class="fab fa-symfony ico" title="Symfony"></i>
+	</div>
 </div>
 
 
+<h3 id="cms">CMS</h3>
+
+<div class="si">
+	<div class="wpti">
+		<i id="wp" class="fab fa-wordpress ico" title="Wordpress"></i>
+	</div>
+</div>
+</div>
+
 </body>
+  <script src="https://kit.fontawesome.com/f3b59627ec.js" crossorigin="anonymous"></script>
 </html>
