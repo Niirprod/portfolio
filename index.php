54d4aaebdefcae6ce4afd5f02e0edf6a739871a9
nabil <djanfar.nabil@gmail.com>
2019-11-19 23:21:36 +0100

    ancre home capabilities...

--- index.php
+++ index.php
@@ -13,63 +13,124 @@
 </head>
 <body>
 	<header>
-		<div id="back"></div>	
 		<div id="fondmenu">
-			
 			<a href="#">		
 				<i id="logo" class="fab fa-jedi-order" title="Acceuil"></i>
 			</a>
 
 			<ul class="menu">
-				<li> <a href="#">A propos</a></li>
-				<li> <a href="#comp">Compétences</a></li>
-				<li> <a href="#">Mes projets</a></li>
+				<li> <a href="#propo">A propos</a></li>
+				<li> <a href="#tech">Technos</a></li>
+				<li> <a href="#proj">Mes projets</a></li>
 				<li> <a href="#"></a></li>
-				<li id="contact"> <a href="#">Me contacter</a> </li>	
+				<li id="contact"> <a href="#cont">Me contacter</a> </li>	
 			</ul>
 		</div>
 	</header>
 
-<div class="top">
-	<h1 id="dwj">Développeur web junior</h1>
-	<h2 id="nd">Nabil DJANFAR</h2>
-</div>
+	
+		<h1 id="dwj">Développeur web junior</h1>
+		<h2 id="nd">Nabil DJANFAR</h2>
+	
+	<div id="propo">
+		<h2 id="moi">A propos de moi</h2>
+		<div class="par">
+		<p id="pres">Je m'appelle DJANFAR NAbil, j'ai 24 ans et je suis en formation développeur web à Simplon.
+			En formation développeur web et web mobile à Simplon Clermont-Ferrand, je cherche à effectuer un stage pour mettre en pratique les connaissances que j'ai acquis, et obtenir mon titre professionnel.
+		</p>
+		</div>
 
-<h2 class="">A propos de mois</h2>
-
-	<p>Je m'appelle DJANFAR NAbil, j'ai 24 ans et je suis en formation développeur web à Simplon.</p>
-
-<div id="comp">
-<h2>Mes compétences</h2>
-
-<div class="ti">
-	<h3 id="fronti">Front end</h3> 
-
-	<h3 id="backti">Back end</h3>
-</div>
-
-<div class="si">
-	<div class="front">
-		<i id="html" class="fab fa-html5 ico" title="html"></i>
-		<i id="css" class="fab fa-css3-alt ico" title="css"></i>
-		<i id="js" class="fab fa-js ico" title="Javascript"></i> 
 	</div>
 
-	<div class="back">
-		<i id="php" class="fab fa-php ico" title="php"></i>
-		<i id="sf" class="fab fa-symfony ico" title="Symfony"></i>
+		
+	
+<div id="tech">
+	<h2 id="mescomp">Technologies</h2>
+
+	<div class="ti">
+		<h3 class="tech_ti">Front end</h3> 
+
+		<h3 class="tech_ti">Back end</h3>
 	</div>
-</div>
 
+	<div class="si">
+		<div class="tech_ico">
+			<figure>
+			<i id="html" class="fab fa-html5 ico" title="html" alt="icone html"></i>
+			<figcaption>html</figcaption>
+		 	</figure>
+		 	<figure>
+			<i id="css" class="fab fa-css3-alt ico" title="css" alt="icone css"></i>
+			<figcaption>css</figcaption>
+		 	</figure>
+		 	<figure>
+			<i id="js" class="fab fa-js ico" title="Javascript" alt="icone Javascript"></i> 
+			<figcaption>javascript</figcaption>
+		 	</figure>
+		</div>
 
-<h3 id="cms">CMS</h3>
+		<div class="tech_ico">
+			<figure>
+				<i id="php" class="fab fa-php ico" title="php" alt="icone php"></i>
+				<figcaption>php</figcaption>
+		 	</figure>
 
-<div class="si">
-	<div class="wpti">
-		<i id="wp" class="fab fa-wordpress ico" title="Wordpress"></i>
+			<figure>
+		 		<i id="mysql" class="fas fa-database ico" title="mysql" alt="icone base de donées"></i>
+		 	 	<figcaption>Mysql</figcaption>
+		 	</figure>
+		</div>
 	</div>
+
+
+	<div class="ti">
+		<h3 class="tech_ti">CMS / Framework</h3>
+		<h3 class="tech_ti">Autres</h3>
+	</div>
+
+	<div class="si">
+	<div class="tech_ico">
+		<figure>
+			<i id="wp" class="fab fa-wordpress ico" title="Wordpress" alt="icone Wordpress"></i>
+			<figcaption>Wordpress</figcaption>
+		</figure>
+
+		<figure>
+			<i id="sf" class="fab fa-symfony ico" title="Symfony" alt="icone Symfony"></i>
+			<figcaption>Symfony</figcaption>
+		</figure>
+	</div>
+
+	<div class="tech_ico">
+		<figure>
+			<img class="othi" src="img/terminal.svg" title="console linux" alt="icone terminal">
+			<figcaption>Terminal Linux</figcaption>
+		</figure>
+		<figure>
+			<img class="othi" src="img/github.svg" title="github" alt="icone github">
+			<figcaption>Github</figcaption>
+		</figure>
+	</div>
+	</div>
+
 </div>
+
+<div id="proj">
+	
+
+
+
 </div>
+
+
+<footer id="footer">
+	
+
+
+</footer>
+
+
+
 
 </body>
   <script src="https://kit.fontawesome.com/f3b59627ec.js" crossorigin="anonymous"></script>
